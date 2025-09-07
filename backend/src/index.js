2fa589cb738fd9146fea03308b8f927fbad78b8f
Rudresh-codes <work.rudreshsuryawanshi@gmail.com>
2025-09-07 21:04:49 +0530

    updated index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,9 +4,13 @@ import app from "./app.js";
 
 const port = process.env.PORT || 4000;
 
-connectDB().then(() => {
-  app.listen(port, () => console.log(`API running on http://localhost:${port}`));
-});
+export default async function handler(req, res) {
+  await connectDB();
+  return app(req, res); // pass control to Express
+}
+// connectDB().then(() => {
+//   app.listen(port, () => console.log(`API running on http://localhost:${port}`));
+// });
 
 app.get("/", (req, res) => {
   res.send("API is running...");
